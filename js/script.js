c04b6681b35d9d01650bcdf42882a0cf8c204f5d
Chrystian Paszkowski <chrispts@wp.pl>
2022-07-26 23:48:40 +0200

    variables

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
+    
 }
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+let computerMove = `kamień`;
+let playerMove = `papier`;
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
