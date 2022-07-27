e005d023b802b5ed5b26f691f7c91d2687cb9f3b
Chrystian Paszkowski <chrispts@wp.pl>
2022-07-27 23:36:14 +0200

    addposibilites

--- js/script.js
+++ js/script.js
@@ -38,3 +38,42 @@ else if(randomNumber == 3){
 
 printMessage('Twój ruch to: ' + playerMove);
 
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}
+
+    else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty przegrywasz!');
+    }
+
+    else if( computerMove == 'kamień' && playerMove == 'kamień'){
+        printMessage('Jest remis!');
+    }
+
+    else if( computerMove == 'papier' && playerMove == 'nożyce'){
+        printMessage('Ty wygrywasz!');
+    }
+
+    else if( computerMove == 'papier' && playerMove == 'kamień'){
+        printMessage('Ty przegrywasz!');
+    }
+
+    else if( computerMove == 'papier' && playerMove == 'papier'){
+        printMessage('Jest remis!');
+    }
+
+    else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+        printMessage('Jest remis!');
+    }
+    
+    else if( computerMove == 'nożyce' && playerMove == 'papier'){
+        printMessage('Ty przegrywasz!');
+    }
+
+    else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+    }
+
+    else( computerMove == 'kamień' && playerMove == 'nożyce'){
+        printMessage('Ty przegrywasz!');
+    }
